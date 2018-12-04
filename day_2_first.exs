75c26569e8cebe238b1b7e1b3b6f242d87ee5ea9
bhois <bernd.hois@bearingpoint.com>
2018-12-04 10:02:05 +0100

    Update

--- day_2_first.exs
+++ day_2_first.exs
@@ -2,20 +2,23 @@
 # https://adventofcode.com/2018/day/2
 defmodule Day2First do
   def run(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn x -> String.graphemes(x) end)
-    |> Enum.map(fn x -> Enum.sort(x) end)
-    |> Enum.map(fn x -> Enum.chunk_by(x, fn x -> x end) end)
-    |> Enum.map(fn x -> Enum.filter(x, fn x -> length(x) > 1 && length(x) < 4 end) end)
-    |> Enum.flat_map(fn x -> x end)
-    |> Enum.reduce(%{two: 0, three: 0}, fn x, %{two: cnt2, three: cnt3} = map ->
-      case length(x) do
-        2 -> Map.put(map, :two, cnt2 + 1)
-        3 -> Map.put(map, :three, cnt3 + 1)
-        _ -> map
-      end
-    end)
+    map = input
+          |> String.split("\n", trim: true)
+          |> Enum.map(fn x -> String.graphemes(x) end)
+          |> Enum.map(fn x -> Enum.sort(x) end)
+          |> Enum.map(fn x -> Enum.chunk_by(x, fn x -> x end) end)
+          |> Enum.map(fn x -> Enum.map(x, fn x -> length(x) end) end)
+          |> Enum.map(fn x -> Enum.uniq(x) end)
+          |> Enum.flat_map(fn x -> x end)
+          |> Enum.reduce(%{two: 0, three: 0}, fn x, %{two: cnt2, three: cnt3} = map ->
+            case x do
+              2 -> Map.put(map, :two, cnt2 + 1)
+              3 -> Map.put(map, :three, cnt3 + 1)
+              _ -> map
+            end
+          end)
+
+      map.two * map.three
   end
 end
 
